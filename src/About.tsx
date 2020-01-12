7fff305ba00961b76ff5f9f0e12847215edd049a
Josh Sprow <jsprow@gmail.com>
2020-01-12 17:56:01 -0500

    Added react-helmet and some silly page title tricks.

--- src/About.tsx
+++ src/About.tsx
@@ -1,14 +1,34 @@
-import React, { FC, lazy } from "react";
+import React, { FC, lazy, useEffect, useState } from "react";
 
+const HelmetTitle = lazy(() => import("./HelmetTitle"));
 const Page = lazy(() => import("./Page"));
 
 const About: FC = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prev => {
+        const next = prev + 1;
+        console.log([prev, next]);
+        return next;
+      });
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <Page className="about-page">
-      <article>
-        <p>This is a PWA!</p>
-      </article>
-    </Page>
+    <>
+      <HelmetTitle page="About" suffix={count} />
+      <Page className="about-page">
+        <article>
+          <p>This is a PWA!</p>
+        </article>
+      </Page>
+    </>
   );
 };
 
