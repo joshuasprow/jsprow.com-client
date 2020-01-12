84c986084e92cd93c5ce61accbfb62d8f3e2b077
Josh Sprow <jsprow@gmail.com>
2020-01-12 18:57:35 -0500

    Attempt to commit with new pre-push hook. Should work

--- src/About.tsx
+++ src/About.tsx
@@ -4,22 +4,9 @@ const HelmetTitle = lazy(() => import("./HelmetTitle"));
 const Page = lazy(() => import("./Page"));
 
 const About: FC = () => {
-  const [count, setCount] = useState(0);
-  const suffix = `${count * 2}s`;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount(prev => prev + 1);
-    }, 2000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <>
-      <HelmetTitle page="About" suffix={suffix} />
+      <HelmetTitle page="About" />
       <Page className="about-page">
         <article>
           <p>This is a PWA!</p>
