be24546c2dd67ead9f968b5a8170dcdfe5fec7ee
Josh Sprow <jsprow@gmail.com>
2020-02-12 17:05:43 -0500

    Trying to implement color mode using chackra ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,79 @@
+import {
+  Box,
+  ColorModeProvider,
+  Flex,
+  IconButton,
+  Link,
+  ThemeProvider,
+  useColorMode
+} from "@chakra-ui/core";
 import React, { FC, lazy, Suspense } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 import GitHubCorner from "./components/GitHubCorner";
-import Navbar from "./components/Navbar";
-import ThemeContext from "./context/ThemeContext";
 
-const BirdConversation = lazy(() => import("./pages/BirdConversation"));
 const Home = lazy(() => import("./pages/Home"));
-const TimerTower = lazy(() => import("./pages/TimerTower"));
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const background = { dark: "gray.600", light: "gray.50" };
+  const color = { dark: "gray.50", light: "gray.900" };
+
+  const buttonBg = colorMode === "dark" ? background.light : background.dark;
+  const buttonColor = colorMode === "dark" ? color.light : color.dark;
+
+  return (
+    <Box
+      as="header"
+      bg={background[colorMode]}
+      borderBottomWidth="1px"
+      color={color[colorMode]}
+      height="4rem"
+      left="0"
+      position="fixed"
+      right="0"
+      top="0"
+      zIndex={4}
+      width="full"
+    >
+      <Flex align="center" height="100%" justify="space-between" paddingX="6">
+        <Link color={color[colorMode]} href="/">
+          jsprow.com
+        </Link>
+        <IconButton
+          aria-label="Toggle color mode"
+          bg={buttonBg}
+          color={buttonColor}
+          icon={colorMode === "light" ? "moon" : "sun"}
+          onClick={toggleColorMode}
+          size="sm"
+        />
+      </Flex>
+    </Box>
+  );
+};
 
 const App: FC = () => {
   return (
     <HelmetProvider>
-      <ThemeContext.Provider>
-        <Router>
-          <Navbar />
-          <Suspense fallback={"Loading..."}>
-            <Switch>
-              <Route component={BirdConversation} path="/bird-conversation" />
-              <Route component={TimerTower} path="/timer-tower" />
-              <Route component={Home} path="/" />
-            </Switch>
-          </Suspense>
-          <GitHubCorner />
-        </Router>
-      </ThemeContext.Provider>
+      <ThemeProvider>
+        <ColorModeProvider>
+          <Router>
+            <Box paddingTop="4rem">
+              <Navbar />
+              <Suspense fallback={"Loading..."}>
+                <Switch>
+                  {/* <Route component={BirdConversation} path="/bird-conversation" /> */}
+                  {/* <Route component={TimerTower} path="/timer-tower" /> */}
+                  <Route component={Home} path="/" />
+                </Switch>
+              </Suspense>
+              <GitHubCorner />
+            </Box>
+          </Router>
+        </ColorModeProvider>
+      </ThemeProvider>
     </HelmetProvider>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { useColorMode } from "@chakra-ui/core";
 import React, { FC } from "react";
 import { RouteComponentProps, useLocation } from "react-router-dom";
 import makeClassName from "../lib/make-page-class-name";
 
 const Home: FC<RouteComponentProps> = () => {
+  const { colorMode } = useColorMode();
+
   const location = useLocation();
   const className = makeClassName(location.pathname);
 
@@ -11,6 +14,7 @@ const Home: FC<RouteComponentProps> = () => {
       <main className={className}>
         <article>
           <p>Welcome to jsprow.com!</p>
+          <pre>{colorMode}</pre>
         </article>
       </main>
     </>
